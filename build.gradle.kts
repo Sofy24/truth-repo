f2a1c04d8bae87c5928c442751156765c2e337c6
Sofia Tosi - sofia.tosi4@studio.unibo.it <sofia.tosi4@studio.unibo.it>
2023-05-02 17:52:27 +0200

    point 5

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,18 +1,32 @@
 plugins {
-java
-scala
-jacoco
+    application
+    java
+    scala
+    jacoco
 }
 repositories {
-mavenCentral()
+    mavenCentral()
 }
 dependencies {
-implementation("org.scala-lang:scala3-library_3:3.2.2")
-val junitVersion = "5.9.3"
-testImplementation("org.junit.jupiter:junit-jupiter-api:$junitVersion")
-testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$junitVersion")
+    implementation("org.scala-lang:scala3-library_3:3.2.2")
+    val junitVersion = "5.9.3"
+    testImplementation("org.junit.jupiter:junit-jupiter-api:$junitVersion")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$junitVersion")
 }
 // Enables JUnit Platform (needed for JUnit 5)
 tasks.named<Test>("test") {
-useJUnitPlatform()
+    useJUnitPlatform()
+}
+
+application {
+// Define the main class for the application.
+    mainClass.set("file")
+}
+
+tasks.test {
+    useJUnitPlatform()
+    testLogging {
+        events(*org.gradle.api.tasks.testing.logging.TestLogEvent.values())
+        showStandardStreams = true
+    }
 }
